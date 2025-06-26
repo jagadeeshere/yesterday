e58cfccf352e69527e3c9b1bd98aba7aa94da921
jagadeeshere <srijagadeesh89@gmail.com>
2025-06-26 13:18:14 +0530

    jag

--- script.js
+++
@@ -1,9 +0,0 @@
-const statuses = ["pending", "in-progress", "succeeded", "failed"];
-
-function updateStatus(element) {
-  let current = statuses.find(status => element.classList.contains(status)) || "pending";
-  let nextIndex = (statuses.indexOf(current) + 1) % statuses.length;
-  
-  element.classList.remove(...statuses);
-  element.classList.add(statuses[nextIndex]);
-}
